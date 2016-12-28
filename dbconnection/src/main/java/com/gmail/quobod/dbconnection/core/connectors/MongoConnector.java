780edea935cbde9cfe63eb35560b42bf92f15162
Rick Walker <ichglauben@gmail.com>
2016-12-28 15:12:40 -0500

    Added call to mongo client close method to fixed the leak issue

--- dbconnection/src/main/java/com/gmail/quobod/dbconnection/core/connectors/MongoConnector.java
+++ dbconnection/src/main/java/com/gmail/quobod/dbconnection/core/connectors/MongoConnector.java
@@ -13,24 +13,32 @@ public class MongoConnector {
 	private String user;
 	private String password;
 	private String databaseName;
-	
-	/**Constructor
-	 * @param hst String database's location
-	 * @param prt String the location's port
-	 * @param db String the database's name
-	 * @param usr String database user
-	 * @param pwd String the user's password*/
+
+	/**
+	 * Constructor
+	 * 
+	 * @param hst
+	 *            String database's location
+	 * @param prt
+	 *            String the location's port
+	 * @param db
+	 *            String the database's name
+	 * @param usr
+	 *            String database user
+	 * @param pwd
+	 *            String the user's password
+	 */
 	public MongoConnector(String hst, String prt, String db, String usr, String pwd) {
 		if (null == hst || null == prt || null == db || null == usr || null == pwd) {
 			throw new NullPointerException(
 					"Expected non-null host url, port database name, user and password\nCheck parameters");
 		}
-		
+
 		if (hst.isEmpty() || prt.isEmpty() || db.isEmpty() || usr.isEmpty() || pwd.isEmpty()) {
 			throw new NullPointerException(
 					"Expected non-empty host url, port database name, user and password\nCheck parameters");
 		}
-		
+
 		this.host = hst;
 		this.port = prt;
 		this.databaseName = db;
@@ -38,25 +46,43 @@ public class MongoConnector {
 		this.password = pwd;
 	}
 
-	/**Connects to the database
-	 * @return String the connection point if successful*/
+	/**
+	 * Connects to the database
+	 * 
+	 * @return String the connection point if successful
+	 */
 	public String connected() {
-//		mongodb://<dbuser>:<dbpassword>@ds049466.mlab.com:49466/mytasks
-//		tester:tester@ds049466.mlab.com:"
-		
-		String url = "mongodb://" + user + ":" + password + "@" + host + ":" + port + "/" + databaseName;
-		
-		MongoClientURI uri = new MongoClientURI(url, MongoClientOptions.builder().cursorFinalizerEnabled(false));
-		
-		MongoClient client = new MongoClient(uri);
-//		System.out.println("Connection Point: " + client.getConnectPoint());
-		return client.getConnectPoint();
+		try {
+			// mongodb://<dbuser>:<dbpassword>@ds049466.mlab.com:49466/mytasks
+			// tester:tester@ds049466.mlab.com:"
+
+			String url = "mongodb://" + user + ":" + password + "@" + host + ":" + port + "/" + databaseName;
+
+			MongoClientURI uri = new MongoClientURI(url, MongoClientOptions.builder().cursorFinalizerEnabled(false));
+
+			mongoClient = new MongoClient(uri);
+			// System.out.println("Connection Point: " +
+			// client.getConnectPoint());
+			return mongoClient.getConnectPoint();
+		} catch (Exception exc) {
+			exc.printStackTrace();
+			return null;
+		} finally {
+			try {
+				mongoClient.close();
+			} catch (Exception exc) {
+				exc.printStackTrace();
+			}
+		}
 	}
-	
-	/**Closes the connection
-	 * @return true after nulling all of the class' variables*/
+
+	/**
+	 * Closes the connection
+	 * 
+	 * @return true after nulling all of the class' variables
+	 */
 	public boolean closeConnection() {
-//		mongoClient.close();
+		// mongoClient.close();
 		mongoClient = null;
 		host = null;
 		port = null;
